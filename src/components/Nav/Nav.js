278091ba5c9a2d4370504ae54f536b82e5d41852
sbk201 <siuym4mym4@gmail.com>
2022-01-31 23:15:37 +0800

    github icon and link

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,11 +2,14 @@ import { useHistory } from "react-router-dom";
 import NavBS from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import github from "../../assets/github.png";
 import styles from "./Nav.module.css";
 
 function Nav() {
     let history = useHistory();
     const toPopular = () => history.push("/");
+    const toGithub = () =>
+        window.open("https://github.com/gutbk201/redux-test", "");
     return (
         <Navbar
             collapseOnSelect
@@ -20,9 +23,19 @@ function Nav() {
                     Movie Lookup
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
+                <Navbar.Collapse
+                    id="responsive-navbar-nav"
+                    className={styles["responsive-navbar-nav"]}
+                >
                     <NavBS className="me-auto"></NavBS>
                     <NavBS className={styles.nav}>
+                        <NavBS.Link href="#" onClick={toGithub}>
+                            <img
+                                src={github}
+                                alt="github logo"
+                                className={styles.github}
+                            />
+                        </NavBS.Link>
                         <NavBS.Link href="#" onClick={toPopular}>
                             Popular
                         </NavBS.Link>
